7d99765057a928d76e8778341bd7091df7dc0d3c
Yucel Parsak <parsak@gmail.com>
2026-05-05 12:58:35 -0400

    more changes

--- scripts/updateGameEvaluation.js
+++ scripts/updateGameEvaluation.js
@@ -122,7 +122,7 @@ async function main() {
             const [evalResult] = await conn.execute(
                `INSERT INTO evaluation (fen,material_eval, positional_eval, final_eval, incheck, mate) 
                VALUES (?,?, ?, ?, ?, ?)`,
-               [move.fen, valData.material, evalData.positional, evalData.final, evalData.in_check, evalData.mate]
+               [move.fen, evalData.material, evalData.positional, evalData.final, evalData.in_check, evalData.mate]
             );
 
             const newEvalId = evalResult.insertId;
